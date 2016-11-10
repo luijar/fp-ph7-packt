d186f89a3dc110ddd99a8ecbd15512df64758bde
luijar <luijar@luisatencio.net>
2016-11-10 17:48:20 -0500

    Sect3

--- src/vol1/sect3/3.1/identity.php
+++ src/vol1/sect3/3.1/identity.php
@@ -11,3 +11,23 @@ namespace Vol1\Sect3\Video1;
 function identity($a) {
 	return $a;
 }
+
+$fruits = array('lemons' => 1, 'oranges' => 4, 'bananas' => 5, 'apples' => 10);
+
+$fruitsArrayObject = new \ArrayObject($fruits);
+$fruitsArrayObject['pears'] = 4;
+
+// create a copy of the array (OOP)
+$copy = $fruitsArrayObject->getArrayCopy();
+print 'First copy (OOP)'. PHP_EOL;
+print_r($copy);
+
+// use identity to create a copy (FP)
+$anotherCopy = array_map(__NAMESPACE__. '\identity', $fruits);
+$anotherCopy['pears'] = 4;
+print 'Second copy (FP)'. PHP_EOL;
+print_r($anotherCopy);
+
+// show original unchanged
+print 'Original (unchanged)'. PHP_EOL;
+print_r($fruits);
