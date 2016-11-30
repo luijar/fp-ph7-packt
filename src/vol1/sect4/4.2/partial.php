20bdb1297290741684bcdaf75f3e99837feee41a
luijar <luijar@luisatencio.net>
2016-11-30 18:13:50 -0500

    Sect4

--- src/vol1/sect4/4.2/partial.php
+++ src/vol1/sect4/4.2/partial.php
@@ -8,10 +8,7 @@
 declare(strict_types=1);
 namespace Vol1\Sect4\Video2;
 
-function partial(/* $func, $args... */): callable {
-    $args = func_get_args();
-    $func = array_shift($args);
-
+function partial($func, ...$args): callable {
     return function() use ($func, $args) {
         return call_user_func_array($func, array_merge($args, func_get_args()));
     };
